cd125a22c0dde11ffd7c9034166a5fd32b493f7d
Tiago370 <tiagoamral23@gmail.com>
2021-06-23 16:03:13 -0300

    novos resultados

--- scripts/processar.py
+++ scripts/processar.py
@@ -6,7 +6,6 @@ os.system("touch dados-treinamento-redes.csv")
 #apendar "Configuraçao,Tamanho,nGerações,Tempo de Treinamento"
 os.system('echo "Configuraçao,Tamanho,nGerações,Tempo de Treinamento" >> dados-treinamento-redes.csv')
 
-"""
 #treinar rede ORS 2x2 homogenea com população de 100, <nGenerations> gerações ou até ficar 200 gerações sem melhoras
 os.system('echo "hom-" >> dados-treinamento-redes.csv')
 os.system("./build/escalonamento train -order -relativize -subtract -net redes/hom-ORS-2x2 -instance treinamento-homogeneo -generations " + str(nGenerations) + " -population 100 -hidden-layers 2 -hidden-neurons 2")
@@ -48,7 +47,6 @@ os.system('echo "het-" >> dados-treinamento-redes.csv')
 os.system("./build/escalonamento train -order -relativize -subtract -net redes/het-ORS-10x10 -instance treinamento-heterogeneo -generations " + str(nGenerations) + " -population 100 -hidden-layers 10 -hidden-neurons 10")
 #apendar "ORS,10x10,<generations>,<Tempo de Treinamento>"
 
-"""
 #sem subtract
 #treinar rede OR 2x2 homogenea com população de 100, <nGenerations> gerações ou até ficar 200 gerações sem melhoras
 os.system('echo "hom-" >> dados-treinamento-redes.csv')
